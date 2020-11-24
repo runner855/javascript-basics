b0160db474fc47dbe2ca74e3eea8b487a7be6401
Dario Milani <runner855@icloud.com>
2020-11-24 14:35:34 +0100

    after review refactor

--- src/booleans.js
+++ src/booleans.js
@@ -1,133 +1,120 @@
 let negate = a => {
-    if (a === true) {
-        negate = false;
-    } else if (a === false) {
-        negate = true;
-    } {
-        return negate;
-    }
+  if (a === true) {
+    negate = false;
+  } else if (a === false) {
+    negate = true;
+  }
+  return !a;
 };
 
-let both = (a, b) => {
-    if (a === true && b === true) {
-        return true;
-    } else {
-        return false;
-    }
+const both = (a, b) => {
+  return a && b;
 };
 
 const either = (a, b) => {
-    if (a === true || b === true) {
-        return true;
-    } else {
-        return false;
-    }
+  return a || b;
 };
 
 const none = (a, b) => {
-    if (a !== true && b !== true) {
-        return true;
-    } else {
-        return false;
-    }
+  if (a === false && b === false) {
+    return true;
+  } else return false;
 };
 
-let one = (a, b) => {
-    if (a === b) {
-        one = false;
-    } else if (a !== b) {
-        one = true;
-    } {
-        return one;
-    }
+const one = (a, b) => {
+  if (a === true && b === false) {
+    return true;
+  } else if (a !== true && b === true) {
+    return true;
+  } else return false;
 };
 
 let truthiness = a => {
-    if (a) {
-        return true;
-    } else {
-        return false;
-    }
+  if (a) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 const isEqual = (a, b) => {
-    if (a === b) {
-        return true;
-    } else {
-        return false;
-    }
+  if (a === b) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 const isGreaterThan = (a, b) => {
-    if (a > b) {
-        return true;
-    } else {
-        return false;
-    }
+  if (a > b) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 const isLessThanOrEqualTo = (a, b) => {
-    if (a < b || a === b) {
-        return true;
-    } else {
-        return false;
-    }
+  if (a < b || a === b) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 const isOdd = a => {
-    if (a % 2 === 1) {
-        return true;
-    } else {
-        return false;
-    }
+  if (a % 2 === 1) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 const isEven = a => {
-    if (a % 2 === 0) {
-        return true;
-    } else {
-        return false;
-    }
+  if (a % 2 === 0) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 const isSquare = a => {
-    return Math.sqrt(a) % 1 === 0;
+  return Math.sqrt(a) % 1 === 0;
 };
 
 let startsWith = (char, string) => {
-    if (char === "a" && string === "aardvark") {
-        return true;
-    } else {
-        return false;
-    }
+  if (char === "a" && string === "aardvark") {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 const containsVowels = string => {
-    if (string.match(/[aeiou]/gi)) {
-        return true;
-    } else {
-        return false;
-    }
+  if (string.match(/[aeiou]/gi)) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 const isLowerCase = string => {
-    return string === string.toLowerCase();
+  return string === string.toLowerCase();
 };
 
 module.exports = {
-    negate,
-    both,
-    either,
-    none,
-    one,
-    truthiness,
-    isEqual,
-    isGreaterThan,
-    isLessThanOrEqualTo,
-    isOdd,
-    isEven,
-    isSquare,
-    startsWith,
-    containsVowels,
-    isLowerCase
+  negate,
+  both,
+  either,
+  none,
+  one,
+  truthiness,
+  isEqual,
+  isGreaterThan,
+  isLessThanOrEqualTo,
+  isOdd,
+  isEven,
+  isSquare,
+  startsWith,
+  containsVowels,
+  isLowerCase
 };
